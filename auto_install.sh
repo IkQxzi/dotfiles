a963d0966bf705cfe32ae677be6a1ba5972d6081
IkQxzi <qxzikr@hotmail.com>
2025-03-13 19:33:41 +0000

    comment changes

--- auto_install.sh
+++ auto_install.sh
@@ -56,11 +56,11 @@ rm -r $HOME/dotfiles
 # nvchad plugins
 file="$HOME/testing/.config/nvim/lua/custom/plugins.lua"
 
-# Check if the file exists
+# Check if plugin file exists 
 if [ ! -f "$file" ]; then
   echo "File does not exist. Creating the file..."
-  mkdir -p "$(dirname "$file")"  # Create the directory if it doesn't exist
-  touch "$file"  # Create the file
+  mkdir -p "$(dirname "$file")"  
+  touch "$file"  
   echo "File created successfully at $file."
 else
   echo "File already exists at $file."
